7efe20ae7d199d1359296405c35ef88ecaa0627a
surajsahu01 <surajsahu6365@gmail.com>
2025-07-10 01:14:54 +0530

    Add new

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.Frontend_URL,
+    origin: 'http://localhost:5173',
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
