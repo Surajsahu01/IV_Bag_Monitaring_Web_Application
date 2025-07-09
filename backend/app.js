38a933b8fe216bd2abbaa5fad53ff230e326423a
surajsahu01 <surajsahu6365@gmail.com>
2025-07-10 01:46:35 +0530

    Add URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.Frontend_URL,
   credentials: true,
 }));
 
--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.Frontend_URL,
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
