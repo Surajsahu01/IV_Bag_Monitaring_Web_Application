b61a94dca899746954e2962c11b15b6e488aa93d
Suraj sahu <84985473+Surajsahu01@users.noreply.github.com>
2025-07-10 11:24:17 +0530

    Update socket.js

--- frontend/src/websocket/socket.js
+++ frontend/src/websocket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:4000', {
+const socket = io('https://iv-bag-monitaring.onrender.com', {
   transports: ['websocket'],
   reconnection: true
 });
